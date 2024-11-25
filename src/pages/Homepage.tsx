f56cfc0f036385108b27277e3ac7f3289d06b0db
ByteBrewer101 <161151563+ByteBrewer101@users.noreply.github.com>
2024-11-25 13:31:49 +0530

    final

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -73,11 +73,13 @@ export default function App() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <div className="border border-gray-300 shadow-lg mb-4">
         <Stage width={400} height={300} ref={stageRef}>
-          <Layer draggable={true} >
+          <Layer draggable={true}>
             {BackgroundImage === "" && (
               <Rect width={500} height={500} fill={bgColor} />
             )}
-            {BackgroundImage && <KonvaImage image={image} width={400} height={300}  />}
+            {BackgroundImage && (
+              <KonvaImage image={image} width={400} height={300} />
+            )}
           </Layer>
           <Layer>
             <KonvaImage
@@ -156,7 +158,7 @@ export default function App() {
           />
 
           <Button variant="outline" onClick={() => setText(tempText)}>
-            Submit
+            Add Text
           </Button>
         </div>
 
@@ -167,13 +169,31 @@ export default function App() {
         />
       </div>
 
-      <div>
+      <div className="flex space-x-4">
         <Button
           className="w-full bg-green-300"
           variant={"outline"}
           onClick={handledownloadImage}
         >
           Download Meme!
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() =>
+            window.open("https://github.com/ByteBrewer101/ChillGuyMemeGen")
+          }
+        >
+          <svg
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            className="size-6 fill-slate-900"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"
+            ></path>
+          </svg>
         </Button>
       </div>
     </div>
